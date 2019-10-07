3745217314b3b8951adbc48551ce8afe588fc9ed
SF <sf@viewar.com>
2019-10-07 11:12:49 +0200

    ui(footer): change stargazer link

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -71,7 +71,7 @@ class Footer extends React.PureComponent {
               className="github-button"
               href={this.props.config.repoUrl}
               data-icon="octicon-star"
-              data-count-href="/facebook/docusaurus/stargazers"
+              data-count-href="/viewar/viewar-cli"
               data-show-count="true"
               data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub"
